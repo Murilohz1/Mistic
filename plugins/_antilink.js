adce081ec64f0738acc4669479b5f85f0419d9f3
khaosZz <murilogodsilva@gmail.com>
2023-08-12 13:03:02 -0300

    Initial commit

---
+++ plugins/_antilink.js
@@ -0,0 +1,30 @@
+const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+export async function before(m, {isAdmin, isBotAdmin}) {
+  if (m.isBaileys && m.fromMe) {
+    return !0;
+  }
+  if (!m.isGroup) return !1;
+  const chat = global.db.data.chats[m.chat];
+  const delet = m.key.participant;
+  const bang = m.key.id;
+  const bot = global.db.data.settings[this.user.jid] || {};
+  const user = `@${m.sender.split`@`[0]}`;
+  const isGroupLink = linkRegex.exec(m.text);
+  const grupo = `https://chat.whatsapp.com`;
+  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*𝙷𝙴𝚈!! 𝙾 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺 𝙴𝚂𝚃𝙰 𝙰𝚃𝙸𝚅𝙾, 𝙼𝙰𝚂 𝚅𝙲 𝙴 𝚄𝙼 𝙰𝙳𝙼*');
+  if (chat.antiLink && isGroupLink && !isAdmin) {
+    if (isBotAdmin) {
+      const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+      if (m.text.includes(linkThisGroup)) return !0;
+    }
+    await this.sendMessage(m.chat, {text: `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*𝙷𝙰𝚂𝚃𝙰 𝙻𝙰 𝚅𝙸𝚂𝚃𝙰 𝙱𝙰𝙱𝚈 👋 ${user} 𝙻𝙸𝙽𝙺𝚂 𝙽𝙰𝙾 𝚂𝙰𝙾 𝙿𝙴𝚁𝙼𝙸𝚃𝙸𝙳𝙾𝚂 𝙰𝚀𝚄𝙸, 𝚅𝙲 𝚂𝙴𝚁𝙰 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾...!!*`, mentions: [m.sender]});
+    if (!isBotAdmin) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙾 𝙱𝙾𝚃 𝙽𝙰𝙾 𝙴 𝙰𝙳𝙼𝙸𝙽, 𝙴 𝙽𝙰𝙾 𝙿𝙾𝙳𝙴 𝚁𝙴𝙼𝙾𝚅𝙴𝚁 𝙴𝙻𝙴*');
+    // await conn.sendButton(m.chat, `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*𝙷𝙰𝚂𝚃𝙰 𝙻𝙰 𝚅𝙸𝚂𝚃𝙰 𝙱𝙰𝙱𝚈 👋, ${await this.getName(m.sender)} 𝚁𝙾𝙼𝙿𝙸𝚂𝚃𝙴𝚂 𝙻𝙰𝚂 𝚁𝙴𝙶𝙻𝙰𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙴𝚁𝙰𝚂 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾...!!*${isBotAdmin ? '' : '\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)
+    if (isBotAdmin && bot.restrict) {
+      await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
+      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (responseb[0].status === '404') return;
+    } else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝚘 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙾 𝙱𝙾𝚃 𝙽𝙰𝙾 𝙰𝚃𝙸𝚅𝙾𝚄 𝙾 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝚃𝙴 𝙴𝙻𝙴 𝙿𝙰𝚁𝙰 𝙰𝚃𝙸𝚅𝙰𝚁*');
+  }
+  return !0;
+}
